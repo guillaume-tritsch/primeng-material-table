611e5e1b760ffe582e8c47599a38fa010cd3363d
guillaume-tritsch <80285644+guillaume-tritsch@users.noreply.github.com>
2023-02-03 17:17:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# primeng-material-table
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeng-tablebasic-demo-95sryj)
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+import { TableModule } from 'primeng/table';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    TableModule,
+    HttpClientModule,
+    FormsModule,
+    MatTableModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [ProductService],
+})
+export class AppModule {}
